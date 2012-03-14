f9c51375cddb23383b8c072005949ce7ad38e4ff
maria.lantin@gmail.com <maria.lantin@gmail.com@cd58bb83-cb8c-9c34-d19f-759d3751f3c8>
2012-03-14 17:07:20 +0000

    Original import

---
+++  osc-kinect/OSCKinect/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define _WINSOCKAPI_    // stops windows.h including winsock.h
+
+#include "targetver.h"
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <tchar.h>
+
+#include "windows.h"
+#include "NuiApi.h"
+
+// TODO: reference additional headers your program requires here
